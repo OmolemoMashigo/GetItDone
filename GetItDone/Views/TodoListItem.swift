81452eee51f850c2745c19baaae42fc0a4a67645
Omolemo Mashigo <omomashigo@gmail.com>
2025-10-25 14:04:11 +0200

    added Views with list item functionality

---
+++ GetItDone/Views/TodoListItem.swift
@@ -0,0 +1,18 @@
+//
+//  TodoListItem.swift
+//  GetItDone
+//
+//  Created by Omolemo Mashigo on 2025/10/25.
+//
+
+import SwiftUI
+
+struct TodoListItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TodoListItem()
+}
